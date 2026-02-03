f18f4c24b3607321ca155d6dedd5e4a1885fa46b
Fernanda Kelly <42969258+FernandaKelly@users.noreply.github.com>
2026-02-03 16:01:35 -0300

    Atualização.

--- Step_3_TRI.R
+++ Step_3_TRI.R
@@ -33,14 +33,14 @@ table_PS_8 <- readxl::read_excel("table_PS_8.xlsx", sheet = "Indicador TRI RS")
 dadosRS_MM <- table_PS_8 %>% 
   dplyr::group_by(atividade) %>% 
   dplyr::mutate(
-                soma_N_MM = slider::slide_sum(soma_N, before = 3, complete = TRUE),
-                qtd_horasHabituais_MM = slider::slide_sum(qtd_horasHabituais, before = 3, complete = TRUE),
-                qtd_horasEfetivas_MM = slider::slide_sum(qtd_horasEfetivas, before = 3, complete = TRUE),
-                VA_RS_MM = slider::slide_sum(VA_RS, before = 3, complete = TRUE),
+                soma_N_MM4 = slider::slide_sum(soma_N, before = 3, complete = TRUE),
+                qtd_horasHabituais_MM4 = slider::slide_sum(qtd_horasHabituais, before = 3, complete = TRUE),
+                qtd_horasEfetivas_MM4 = slider::slide_sum(qtd_horasEfetivas, before = 3, complete = TRUE),
+                VA_RS_MM4 = slider::slide_sum(VA_RS, before = 3, complete = TRUE),
                 
-                indicadorVA_N_MM4 = VA_RS_MM/soma_N_MM,
-                indicadorVA_qtd_HHabituais_MM4 = VA_RS_MM/(qtd_horasHabituais_MM*12.9),
-                indicadorVA_qtd_HEfetivas_MM4 = VA_RS_MM/(qtd_horasEfetivas_MM*12.9))
+                indicadorVA_N_MM4 = VA_RS_MM4/soma_N_MM4,
+                indicadorVA_qtd_HHabituais_MM4 = VA_RS_MM4/(qtd_horasHabituais_MM4*12.9),
+                indicadorVA_qtd_HEfetivas_MM4 = VA_RS_MM4/(qtd_horasEfetivas_MM4*12.9))
 
 #####################################################
 # BRASIL
@@ -51,11 +51,19 @@ table_PS_8 <- readxl::read_excel("table_PS_8.xlsx", sheet = "Indicador TRI BR")
 dadosBR_MM <- table_PS_8 %>% 
   dplyr::group_by(atividade) %>% 
   dplyr::mutate(
-    soma_N_MM = slider::slide_sum(soma_N, before = 3, complete = TRUE),
-    qtd_horasHabituais_MM = slider::slide_sum(qtd_horasHabituais, before = 3, complete = TRUE),
-    qtd_horasEfetivas_MM = slider::slide_sum(qtd_horasEfetivas, before = 3, complete = TRUE),
-    VA_BR_MM = slider::slide_sum(VA_BR, before = 3, complete = TRUE),
+    soma_N_MM4 = slider::slide_sum(soma_N, before = 3, complete = TRUE),
+    qtd_horasHabituais_MM4 = slider::slide_sum(qtd_horasHabituais, before = 3, complete = TRUE),
+    qtd_horasEfetivas_MM4 = slider::slide_sum(qtd_horasEfetivas, before = 3, complete = TRUE),
+    VA_BR_MM4 = slider::slide_sum(VA_BR, before = 3, complete = TRUE),
     
-    indicadorVA_N_MM4 = VA_BR_MM/soma_N_MM,
-    indicadorVA_qtd_HHabituais_MM4 = VA_BR_MM/(qtd_horasHabituais_MM*12.9),
-    indicadorVA_qtd_HEfetivas_MM4 = VA_BR_MM/(qtd_horasEfetivas_MM*12.9))
+    indicadorVA_N_MM4 = VA_BR_MM4/soma_N_MM4,
+    indicadorVA_qtd_HHabituais_MM4 = VA_BR_MM4/(qtd_horasHabituais_MM4*12.9),
+    indicadorVA_qtd_HEfetivas_MM4 = VA_BR_MM4/(qtd_horasEfetivas_MM4*12.9))
+
+#####################################################
+
+sheets <- list("MM4 RS TRI"   = dadosRS_MM,
+               "MM4 BR TRI"   = dadosBR_MM)
+
+writexl::write_xlsx(sheets, 
+                    paste0("C:/Users/fernanda-romeiro/OneDrive - Governo do Estado do Rio Grande do Sul/Projetos/PNAD/PNAD_projetos/Dados/table_PS_MM_1.xlsx"))

048498cf9aa81532b2e5af11bd6963ab1a431390
Fernanda Kelly <42969258+FernandaKelly@users.noreply.github.com>
2026-03-25 10:01:36 -0300

    Atualização de dados.

---
+++ Step_2_PROD.R
@@ -0,0 +1,29 @@
+#####################################################
+# ENTREGA: INDICADOR DE PRODUTIVIDADE
+# AGRUPADO: --
+# ANOS: 2012 - 2024
+# PERÍODO: ANUAL
+#####################################################
+options(timeout = 600) 
+options(scipen = 999)
+# install.packages("PNADcIBGE")
+# install.packages("survey")
+library(PNADcIBGE)
+library(survey)
+library(foreign)
+library(srvyr)
+library(reactable)
+library(purrr)
+library(tidyverse)
+library(zoo)
+library(slider)
+#####################################################
+#        CONFIGURANDO DIRETÓRIO DE DADOS
+#####################################################
+
+library(here)
+here::set_here("C:/Users/fernanda-romeiro/OneDrive - Governo do Estado do Rio Grande do Sul/Projetos/PNAD/PNAD_projetos/Dados")
+
+#####################################################
+#                LEITURA DAS BASES
+#####################################################
